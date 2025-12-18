5ffef0dc705a5b36d7fbdb10df25c61bdd45547f
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2025-12-18 16:04:45 +0000

    Add editorconfig and version.json (#1)

--- WoofWare.LiangHyphenation.Test/TestSurface.fs
+++ WoofWare.LiangHyphenation.Test/TestSurface.fs
@@ -10,7 +10,7 @@ module TestSurface =
     [<Test>]
     let ``Ensure API surface has not been modified`` () = ApiSurface.assertIdentical assembly
 
-    [<Test; Explicit>]
+    [<Test ; Explicit>]
     let ``Update API surface`` () =
         ApiSurface.writeAssemblyBaseline assembly
 
@@ -18,7 +18,7 @@ module TestSurface =
     let ``Ensure public API is fully documented`` () =
         DocCoverage.assertFullyDocumented assembly
 
-    [<Test; Explicit "Not yet published">]
+    [<Test ; Explicit "Not yet published">]
     // https://github.com/nunit/nunit3-vs-adapter/issues/876
     let ``EnsureVersionIsMonotonic`` () =
         MonotonicVersion.validate assembly "WoofWare.LiangHyphenation"
